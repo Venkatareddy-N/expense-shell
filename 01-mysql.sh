cd16534bbb4d3f75c8aa3a6f43364ebd01cee330
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-07 13:35:10 +0530

    Expense project with shell scripting

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+TIME_STAMP=$(date +%Y-%M-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 
-USER_ID=$(id -u)
+mkdir -p $LOGS_FOLDER # -p if dir not there it will create 
 
+USER_ID=$(id -u) # 0-for root user
+
+#COLOR Codes
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-USAGE(){
-    echo -e "$R USAGE:: $N sudo sh $0 package1 package2 ..."
-    exit 1
-}
-
 CHECK_ROOT(){
     if [ $USER_ID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
-        exit 1
+        echo -e "$R please run script with root user $N" | tee -a $LOG_FILE
+        exit 1 # exit running script form this point
     fi
 }
+
+CHECK_ROOT
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -35,8 +34,6 @@ VALIDATE(){
         echo -e "$2 is $G SUCCESS $N"
     fi
 }
-
-CHECK_ROOT
 
 dnf install mysql-server -y
 VALIDATE $1 "Installing mysql-server"
