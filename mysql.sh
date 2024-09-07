1ddce0f7016a322516fa27c020e76ac9e3b204ab
venkatswan1991 <venkatswan1991@gmail.com>
2024-09-07 12:57:22 +0530

    Expense project with shell scripting

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-LOGS_FOLDER=/var/log/expense
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%M-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 
-echo "Logs Directory is : $LOGS_FOLDER"
+mkdir -p $LOGS_FOLDER # -p if dir not there it will create 
+
+
+USER_ID=$(id -u) # 0-for root user
+
+echo "Logs file is : $LOG_FILE and user id is : $USER_ID"
